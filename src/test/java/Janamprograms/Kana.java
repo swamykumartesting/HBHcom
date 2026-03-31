5a212d1878e2a0f470817ef1f24210afb7156f8a
Charvik <Charvik@LAPTOP-S0IS0QVR>
2026-03-31 09:00:48 +0530

    My maven first execution

---
+++ src/test/java/Janamprograms/Kana.java
@@ -0,0 +1,19 @@
+package Janamprograms;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Kana {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
